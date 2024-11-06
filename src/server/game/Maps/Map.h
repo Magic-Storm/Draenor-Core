ddbbd874316182ba8629e7ee0b54aa8ec92f5975
welder1976 <zemetskia@gmail.com>
2024-11-06 20:36:15 +0300

    Core/Maps: move GridMap out of Map.h/cpp

--- src/server/game/Maps/Map.h
+++ src/server/game/Maps/Map.h
@@ -34,6 +34,7 @@ class WorldObject;
 class TempSummon;
 class Player;
 class CreatureGroup;
+class GridMap;
 struct ScriptInfo;
 struct ScriptAction;
 struct Position;
@@ -51,20 +52,14 @@ struct ScriptAction
     ScriptInfo const* script;                               // pointer to static script data
 };
 
-union u_map_magic
-{
-    char asChar[4];
-    uint32 asUInt;
-};
-
 // ******************************************
 // Map file format defines
 // ******************************************
 struct map_fileheader
 {
-    u_map_magic mapMagic;
-    u_map_magic versionMagic;
-    u_map_magic buildMagic;
+    uint32 mapMagic;
+    uint32 versionMagic;
+    uint32 buildMagic;
     uint32 areaMapOffset;
     uint32 areaMapSize;
     uint32 heightMapOffset;
@@ -148,67 +143,6 @@ enum InstanceLockTypes
 	INSTANCE_LOCK_STRICT,        // Used for: Vanilla + TBC raids, WOTLK Heroic raids, MOP Heroic raids excluding SoO, Mythic diff raids in WOD.
 	INSTANCE_LOCK_FLEXIBLE,      // Used for: Normal WotLK, MoP raids. SoO excluded.
 	INSTANCE_LOCK_LOOT_BASED     // Used for: LFR, Flex mode, SoO NM/HC, WoD NM/HC.
-};
-
-class GridMap
-{
-    uint32  _flags;
-    union{
-        float* m_V9;
-        uint16* m_uint16_V9;
-        uint8* m_uint8_V9;
-    };
-    union{
-        float* m_V8;
-        uint16* m_uint16_V8;
-        uint8* m_uint8_V8;
-    };
-    int16* _maxHeight;
-    int16* _minHeight;
-    // Height level data
-    float _gridHeight;
-    float _gridIntHeightMultiplier;
-
-    // Area data
-    uint16* _areaMap;
-
-    // Liquid data
-    float _liquidLevel;
-    uint16* _liquidEntry;
-    uint8* _liquidFlags;
-    float* _liquidMap;
-    uint16 _gridArea;
-    uint16 _liquidType;
-    uint8 _liquidOffX;
-    uint8 _liquidOffY;
-    uint8 _liquidWidth;
-    uint8 _liquidHeight;
-
-
-    bool loadAreaData(FILE* in, uint32 offset, uint32 size);
-    bool loadHeihgtData(FILE* in, uint32 offset, uint32 size);
-    bool loadLiquidData(FILE* in, uint32 offset, uint32 size);
-
-    // Get height functions and pointers
-    typedef float (GridMap::*GetHeightPtr) (float x, float y) const;
-    GetHeightPtr _gridGetHeight;
-    float getHeightFromFloat(float x, float y) const;
-    float getHeightFromUint16(float x, float y) const;
-    float getHeightFromUint8(float x, float y) const;
-    float getHeightFromFlat(float x, float y) const;
-
-public:
-    GridMap();
-    ~GridMap();
-    bool loadData(char* filaname);
-    void unloadData();
-
-    uint16 getArea(float x, float y) const;
-    inline float getHeight(float x, float y) const {return (this->*_gridGetHeight)(x, y);}
-    float getMinHeight(float x, float y) const;
-    float getLiquidLevel(float x, float y) const;
-    uint8 getTerrainType(float x, float y) const;
-    ZLiquidStatus getLiquidStatus(float x, float y, float z, uint8 ReqLiquidType, LiquidData* data = 0);
 };
 
 // GCC have alternative #pragma pack(N) syntax and old gcc version not support pack(push, N), also any gcc version not support it at some platform
@@ -542,6 +476,7 @@ class Map : public GridRefManager<NGridType>
         void LoadMapAndVMap(int gx, int gy);
         void LoadVMap(int gx, int gy);
         void LoadMap(int gx, int gy, bool reload = false);
+        static void LoadMapImpl(Map* map, int gx, int gy, bool reload);
         void LoadMMap(int gx, int gy);
 
         void SetTimer(uint32 t) { i_gridExpiry = t < MIN_GRID_DELAY ? MIN_GRID_DELAY : t; }
