c8456e5d7746037fe9212036dea3be41ad650150
welder1976 <zemetskia@gmail.com>
2025-02-09 20:04:28 +0300

    + error fix

--- src/server/shared/Cryptography/ARC4.cpp
+++ src/server/shared/Cryptography/ARC4.cpp
@@ -10,7 +10,7 @@
 #include <openssl/sha.h>
 #include "Errors.h"
 
-ARC4::ARC4(uint32 len) : _ctx(EVP_CIPHER_CTX_new())
+ARC4::ARC4(uint32 len) : m_ctx(EVP_CIPHER_CTX_new())
 {
 #if OPENSSL_VERSION_NUMBER >= 0x30000000L
     _cipher = EVP_CIPHER_fetch(nullptr, "RC4", nullptr);
@@ -18,12 +18,12 @@ ARC4::ARC4(uint32 len) : _ctx(EVP_CIPHER_CTX_new())
     EVP_CIPHER const* _cipher = EVP_rc4();
 #endif
 
-    EVP_CIPHER_CTX_init(_ctx);
-    EVP_EncryptInit_ex(_ctx, EVP_rc4(), nullptr, nullptr, nullptr);
-    EVP_CIPHER_CTX_set_key_length(_ctx, len);
+    EVP_CIPHER_CTX_init(m_ctx);
+    EVP_EncryptInit_ex(m_ctx, EVP_rc4(), nullptr, nullptr, nullptr);
+    EVP_CIPHER_CTX_set_key_length(m_ctx, len);
 }
 
-ARC4::ARC4(uint8* seed, uint32 len) : _ctx(EVP_CIPHER_CTX_new())
+ARC4::ARC4(uint8* seed, uint32 len) : m_ctx(EVP_CIPHER_CTX_new())
 {
 #if OPENSSL_VERSION_NUMBER >= 0x30000000L
     _cipher = EVP_CIPHER_fetch(nullptr, "RC4", nullptr);
@@ -31,15 +31,15 @@ ARC4::ARC4(uint8* seed, uint32 len) : _ctx(EVP_CIPHER_CTX_new())
     EVP_CIPHER const* _cipher = EVP_rc4();
 #endif
 
-    EVP_CIPHER_CTX_init(_ctx);
-    EVP_EncryptInit_ex(_ctx, EVP_rc4(), nullptr, nullptr, nullptr);
-    EVP_CIPHER_CTX_set_key_length(_ctx, len);
-    EVP_EncryptInit_ex(_ctx, nullptr, nullptr, seed, nullptr);
+    EVP_CIPHER_CTX_init(m_ctx);
+    EVP_EncryptInit_ex(m_ctx, EVP_rc4(), nullptr, nullptr, nullptr);
+    EVP_CIPHER_CTX_set_key_length(m_ctx, len);
+    EVP_EncryptInit_ex(m_ctx, nullptr, nullptr, seed, nullptr);
 }
 
 ARC4::~ARC4()
 {
-    EVP_CIPHER_CTX_free(_ctx);
+    EVP_CIPHER_CTX_free(m_ctx);
 #if OPENSSL_VERSION_NUMBER >= 0x30000000L
     EVP_CIPHER_free(_cipher);
 #endif
@@ -47,12 +47,12 @@ ARC4::~ARC4()
 
 void ARC4::Init(uint8* seed)
 {
-    EVP_EncryptInit_ex(_ctx, nullptr, nullptr, seed, nullptr);
+    EVP_EncryptInit_ex(m_ctx, nullptr, nullptr, seed, nullptr);
 }
 
 void ARC4::UpdateData(int len, uint8* data)
 {
     int outlen = 0;
-    EVP_EncryptUpdate(_ctx, data, &outlen, data, len);
-    EVP_EncryptFinal_ex(_ctx, data, &outlen);
+    EVP_EncryptUpdate(m_ctx, data, &outlen, data, len);
+    EVP_EncryptFinal_ex(m_ctx, data, &outlen);
 }
