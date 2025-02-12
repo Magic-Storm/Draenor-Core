7cf3c2cd6e44a56b992edfc6eec0cfa8805384cc
welder1976 <zemetskia@gmail.com>
2025-02-12 21:45:25 +0300

    Maybe fix error Cmake

--- cmake/genrev.cmake
+++ cmake/genrev.cmake
@@ -109,10 +109,11 @@ endif()
 
 # Create the actual revision_data.h file from the above params
 # For package/copyright information we always need a proper date - keep "Archived/1970" for displaying git info but a valid year elsewhere
-string(REGEX MATCH "([0-9]+)-([0-9]+)-([0-9]+)" rev_date_fallback_match ${rev_date_fallback})
+STRING(REGEX MATCH "[^0-9]*[0-9]+\\.[0-9]+\\.[0-9]+.*)" rev_date_fallback_match "${rev_date_fallback}")
 set(rev_year ${CMAKE_MATCH_1})
 set(rev_month ${CMAKE_MATCH_2})
 set(rev_day ${CMAKE_MATCH_3})
+
 # Create the actual revision.h file from the above params
 if(NOT "${rev_hash_cached}" STREQUAL "${rev_hash}" OR NOT "${rev_branch_cached}" STREQUAL "${rev_branch}" OR NOT EXISTS "${BUILDDIR}/revision.h")
   configure_file(
